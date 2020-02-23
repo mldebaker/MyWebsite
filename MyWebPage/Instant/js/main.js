0a6916f7c8bd51fb8977c33895143df43930983b
mldebaker <mldebaker@gmail.com>
2020-02-23 16:32:12 -0700

    header update

--- MyWebPage/Instant/js/main.js
+++ MyWebPage/Instant/js/main.js
@@ -1,3 +1,13 @@
 jQuery(document).ready(function( $ ) {
-  
+    Email.send({
+        Host : "smtp.yourisp.com",
+        Username : "username",
+        Password : "password",
+        To : 'them@website.com',
+        From : "you@isp.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+    message => alert(message)
+    );
 });
