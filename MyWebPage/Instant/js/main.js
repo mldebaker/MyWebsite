23e975951bb6e30eff61c419e2589f33e655cf9d
mldebaker <mldebaker@gmail.com>
2020-02-23 19:51:16 -0700

    about page update

--- MyWebPage/Instant/js/main.js
+++ MyWebPage/Instant/js/main.js
@@ -1,13 +1,13 @@
-jQuery(document).ready(function( $ ) {
-    Email.send({
-        Host : "smtp.yourisp.com",
-        Username : "username",
-        Password : "password",
-        To : 'them@website.com',
-        From : "you@isp.com",
-        Subject : "This is the subject",
-        Body : "And this is the body"
-    }).then(
-    message => alert(message)
-    );
-});
+
+Email .send({
+    Host : "smtp.elasticemail.com",
+    Username : "mldebaker@gmail.com",
+    Password : "AF6BA840EE7507B0EA2F39E7842C46F11067",
+    To : 'them@website.com',
+    From : "you@isp.com",
+    Subject : "This is the subject",
+    Body : "And this is the body"
+}).then(
+message => alert(message)
+);
+
